1de85d7a4f0e601f8e76dbac1aa0bd5e8046ad08
Martin Karugaba <martinkarugaba21@gmail.com>
2024-02-26 19:29:03 +0300

    Add images with the Image component

--- App.js
+++ App.js
@@ -1,9 +1,20 @@
-import { View, Text } from 'react-native';
+import { View, Text, Image, ImageBackground } from 'react-native';
+const logoImg = require('./assets/adaptive-icon.png');
 
 export default function App() {
   return (
-    <View style={{ flex: 1, backgroundColor: 'plum', padding:60 }}>
-      <Text><Text style={{color: 'white'}}>Hello</Text> World</Text>
+    <View style={{ flex: 1, backgroundColor: 'plum', padding: 60 }}>
+      <Text>
+        <Text style={{ color: 'white' }}>Hello</Text> World
+      </Text>
+      <Image
+        source={logoImg}
+        style={{ height: 300, width: 300, marginTop: 30 }}
+      />
+      <Image
+        source={{uri: 'https://picsum.photos/300'}}
+        style={{ height: 300, width: 300, marginTop: 30 }}
+      />
     </View>
   );
 }
