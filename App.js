b80ed96b0d52ebcf99df5454af600a78bbb555ab
Martin Karugaba <martinkarugaba21@gmail.com>
2024-02-26 20:18:20 +0300

    Add button component

--- App.js
+++ App.js
@@ -1,27 +1,46 @@
-import { View, Text, Image, ImageBackground } from 'react-native';
-const logoImg = require('./assets/adaptive-icon.png');
+import {
+  View,
+  Text,
+  Image,
+  ScrollView,
+  ImageBackground,
+  Button,
+} from 'react-native';
+const logoImg = require('./assets/twitter.jpg');
 
 export default function App() {
   return (
-    <View style={{ flex: 1, backgroundColor: 'plum', padding: 60 }}>
-      {/* <Text>
-        <Text style={{ color: 'white' }}>Hello</Text> React Native
-      </Text>
-      <Image
-        source={logoImg}
-        style={{ height: 300, width: 300, marginTop: 30 }}
-      />
-      <Image
-        source={{uri: 'https://picsum.photos/300'}}
-        style={{ height: 300, width: 300, marginTop: 30 }}
-      /> */}
+    <View style={{ flex: 1, backgroundColor: '#fafafa', padding: 30 }}>
+      <ScrollView>
+        <Text>
+          <Text style={{ color: 'orange' }}>Hello</Text> React Native
+        </Text>
+        <Image source={logoImg} style={{ height: 300, width: 300 }} />
+        <Text>
+          It is a long established fact that a reader will be distracted by the
+          readable content of a page when looking at its layout. The point of
+          using Lorem Ipsum is that it has a more-or-less normal distribution of
+          letters, as opposed to using 'Content here, content here', making it
+          look like readable English. Many desktop publishing packages and web
+          page editors now use Lorem Ipsum as their default model text, and a
+          search for 'lorem ipsum' will uncover many web sites still in their
+          infancy. Various versions have evolved over the years, sometimes by
+          accident, sometimes on purpose (injected humour and the like). Lorem
+          Ipsum is that it has a more-or-less normal distribution of letters, as
+          opposed to using 'Content here, content here', making it look like
+          readable English. Many desktop publishing packages and web page
+          editors now use Lorem Ipsum as their default model text, and a search
+          for 'lorem ipsum' will uncover many web sites still in their infancy.
+          Various versions have evolved over the years, sometimes by accident,
+          sometimes on purpose (injected humour and the like).
+        </Text>
 
-      <ImageBackground
-        source={logoImg}
-        style={{ flex: 1, border: '2px solid black' }}
-      >
-        <Text>IMAGE TEXT</Text>
-      </ImageBackground>
+        <Button
+          title="press"
+          onPress={() => console.log('Hello React Native')}
+          color="amber"
+        />
+      </ScrollView>
     </View>
   );
 }
