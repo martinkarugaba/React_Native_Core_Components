fa67995c8302f035f1f3ac5f95ff05c0c138a999
Martin Karugaba <martinkarugaba21@gmail.com>
2024-02-26 19:36:39 +0300

    Add image background with the ImageBackground component

--- App.js
+++ App.js
@@ -4,8 +4,8 @@ const logoImg = require('./assets/adaptive-icon.png');
 export default function App() {
   return (
     <View style={{ flex: 1, backgroundColor: 'plum', padding: 60 }}>
-      <Text>
-        <Text style={{ color: 'white' }}>Hello</Text> World
+      {/* <Text>
+        <Text style={{ color: 'white' }}>Hello</Text> React Native
       </Text>
       <Image
         source={logoImg}
@@ -14,7 +14,14 @@ export default function App() {
       <Image
         source={{uri: 'https://picsum.photos/300'}}
         style={{ height: 300, width: 300, marginTop: 30 }}
-      />
+      /> */}
+
+      <ImageBackground
+        source={logoImg}
+        style={{ flex: 1, border: '2px solid black' }}
+      >
+        <Text>IMAGE TEXT</Text>
+      </ImageBackground>
     </View>
   );
 }
